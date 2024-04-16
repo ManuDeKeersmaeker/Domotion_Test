af6a482b7769f2eb787ae2684f59360baa58d9b3
ManuDeKeersmaeker <manu.dekeersmaeker1@gmail.com>
2024-04-16 09:36:35 +0200

    Kleuren aan de knoppen toegevoegd + commentaar. De kleuren dienen voor het actief en inactief te zetten van de kasten.

--- SchermKasten.php
+++ SchermKasten.php
@@ -2,8 +2,15 @@
 Dit project is de testomgeving voor het schoolproject Domotion. De bedoeling hiervan
 is dat er verschillende kasten te zien zijn en men virtueel materiaal uit de kasten kan nemen.
 
+Handige variabelen (Vul op xx in de kastnummer in (bv: 01, 02 ,...)):
+$_SESSION['BadgeId'] ==> Dit is het badge id dat de gebruiker heeft ingegeven of dat gescant is
+$_SESSION['KleurKastxx'] ==> Dit is de kleur van de knop.  Mogelijke kleure "red" en "green".
+$_SESSION['Kastxx'] ==> Dit geeft aan of de kast "Open" of "Gesloten" is.
+$_SESSION['StatusKastxx'] ==> Dit geeft aan of de kast "Leeg" of "Vol" is.
+
 Gemaakt door: Manu De Keersmaeker
 -->
+
 
 <?php
 //Hieronder wordt ervoor gezorgdt dat de inhoud van de kast kan veranderen en dat de kast open en dicht kan.
@@ -113,10 +120,19 @@ if (isset($_POST['StatusKast06'])){
         $_SESSION['StatusKast06'] = "Vol";
     }
 }
+
 if ($_SESSION['BadgeId'] == null){
     $_SESSION['BadgeId'] =  $_POST['BadgeId'];
 }
-echo $_SESSION['BadgeId'];
+
+if ($_SESSION['KleurKast01'] == null){
+    $_SESSION['KleurKast01'] = "green";
+    $_SESSION['KleurKast02'] = "green";
+    $_SESSION['KleurKast03'] = "green";
+    $_SESSION['KleurKast04'] = "green";
+    $_SESSION['KleurKast05'] = "green";
+    $_SESSION['KleurKast06'] = "green";
+}
 ?>
 
 
@@ -127,27 +143,27 @@ echo $_SESSION['BadgeId'];
     <table border = 5>
         <tr>
             <td>Kast 1</td>
-            <td><input type = 'submit' name = 'Kast01' value = '<?php echo $_SESSION['Kast01']; ?>'></td>
-            <td><input type = 'submit' name = 'StatusKast01' value = '<?php  echo $_SESSION['StatusKast01'] ?>'></td>
+            <td><input type = 'submit' name = 'Kast01' value = '<?php echo $_SESSION['Kast01']; ?>' style="background-color: <?php echo $_SESSION['KleurKast01'] ?>"></td>
+            <td><input type = 'submit' name = 'StatusKast01' value = '<?php  echo $_SESSION['StatusKast01'] ?>' style="background-color: <?php echo $_SESSION['KleurKast01'] ?>"></td>
             <td>Kast 2</td>
-            <td><input type = 'submit' name = 'Kast02' value = '<?php echo $_SESSION['Kast02']; ?>'></td>
-            <td><input type = 'submit' name = 'StatusKast02' value = '<?php  echo $_SESSION['StatusKast02'] ?>'></td>
+            <td><input type = 'submit' name = 'Kast02' value = '<?php echo $_SESSION['Kast02']; ?>' style="background-color: <?php echo $_SESSION['KleurKast02'] ?>"></td>
+            <td><input type = 'submit' name = 'StatusKast02' value = '<?php  echo $_SESSION['StatusKast02'] ?>' style="background-color: <?php echo $_SESSION['KleurKast02'] ?>"></td>
         </tr>
         <tr>
             <td>Kast 3</td>
-            <td><input type = 'submit' name = 'Kast03' value = '<?php echo $_SESSION['Kast03']; ?>'</td>
-            <td><input type = 'submit' name = 'StatusKast03' value = '<?php  echo $_SESSION['StatusKast03'] ?>'></td>
+            <td><input type = 'submit' name = 'Kast03' value = '<?php echo $_SESSION['Kast03']; ?>' style="background-color: <?php echo $_SESSION['KleurKast03'] ?>"</td>
+            <td><input type = 'submit' name = 'StatusKast03' value = '<?php  echo $_SESSION['StatusKast03'] ?>' style="background-color: <?php echo $_SESSION['KleurKast03'] ?>"></td>
             <td>Kast 4</td>
-            <td><input type = 'submit' name = 'Kast04' value = '<?php echo $_SESSION['Kast04']; ?>'></td>
-            <td><input type = 'submit' name = 'StatusKast04' value = '<?php  echo $_SESSION['StatusKast04'] ?>'></td>
+            <td><input type = 'submit' name = 'Kast04' value = '<?php echo $_SESSION['Kast04']; ?>'style="background-color: <?php echo $_SESSION['KleurKast04'] ?>"></td>
+            <td><input type = 'submit' name = 'StatusKast04' value = '<?php  echo $_SESSION['StatusKast04'] ?>' style="background-color: <?php echo $_SESSION['KleurKast04'] ?>"></td>
         </tr>
         <tr>
             <td>Kast 5</td>
-            <td><input type = 'submit' name = 'Kast05' value = '<?php echo $_SESSION['Kast05']; ?>'></td>
-            <td><input type = 'submit' name = 'StatusKast05' value = '<?php  echo $_SESSION['StatusKast05'] ?>'></td>
+            <td><input type = 'submit' name = 'Kast05' value = '<?php echo $_SESSION['Kast05']; ?>' style="background-color: <?php echo $_SESSION['KleurKast05'] ?>"></td>
+            <td><input type = 'submit' name = 'StatusKast05' value = '<?php  echo $_SESSION['StatusKast05'] ?>' style="background-color: <?php echo $_SESSION['KleurKast05'] ?>"></td>
             <td>Kast 6</td>
-            <td><input type = 'submit' name = 'Kast06' value = '<?php echo $_SESSION['Kast06'] ?>'></td>
-            <td><input type = 'submit' name = 'StatusKast06' value = '<?php  echo $_SESSION['StatusKast06'] ?>'></td>
+            <td><input type = 'submit' name = 'Kast06' value = '<?php echo $_SESSION['Kast06'] ?>' style="background-color: <?php echo $_SESSION['KleurKast06'] ?>"></td>
+            <td><input type = 'submit' name = 'StatusKast06' value = '<?php  echo $_SESSION['StatusKast06'] ?>' style="background-color: <?php echo $_SESSION['KleurKast06'] ?>"></td>
         </tr>
     </table>
 </form>
